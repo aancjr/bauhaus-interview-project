1abe1a6168c2d81c0fc0d9c53638570f4f7f4107
aancjr <andrewancjr@gmail.com>
2022-10-27 07:32:05 -0400

    updated api error handling and added message on screen

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,10 +12,6 @@ const Select = styled.select`
   border-radius: 8px;
 
   option {
-    color: black;
-    background: white;
-    display: flex;
-    white-space: pre;
     min-height: 20px;
     padding: 0px 2px 1px;
   }
@@ -33,9 +29,8 @@ export default function Home() {
                 setData(results.data);
                 let uniqueFilterOptions = [... new Set(results.data.map(obj => obj.category))]
                 setFilterOptions(uniqueFilterOptions);
-                console.log('hey')
             })
-            .catch(err => setError(err));
+            .catch(err => setError(true));
 
         return () => { }
 
@@ -46,33 +41,43 @@ export default function Home() {
     };
 
     return (
-        <div className='container'>
-            <div className="filter-section">
-                <div className="input-row">
-                    <form id="category-filter">
-                        <label >Filter By: </label>
-                        <Select id='images' name='images' onChange={handleChange}>
-                            <option key='default' value=''>All Categories</option>
-                            {filterOptions.map((category, idx) => { return <option key={idx} value={category}>{category}</option> })}
-                        </Select>
-                    </form>
+        <>
+            {!error ? <div className='container'>
+                <div className='filter-section'>
+                    <div className='input-row'>
+                        <form id='category-filter'>
+                            <label >Filter By: </label>
+                            <Select id='images' name='images' onChange={handleChange}>
+                                <option key='default' value=''>All Categories</option>
+                                {filterOptions.map((category, idx) => { return <option key={idx} value={category}>{category}</option> })}
+                            </Select>
+                        </form>
+                    </div>
+                </div>
+                <div className='image-section'>
+                    <div className='image-row'>
+                        {data?.filter(obj => {
+                            return obj.category.includes(pickedFilter)
+                        }).map((image, idx) => (
+                            <ImageCard
+                                category={image.category}
+                                description={image.description}
+                                imageURL={image.image}
+                                key={idx}
+                                title={image.title}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
-            <div className="image-section">
-                <div className="image-row">
-                    {data?.filter(obj => {
-                        return obj.category.includes(pickedFilter)
-                    }).map((image, idx) => (
-                        <ImageCard
-                            category={image.category}
-                            description={image.description}
-                            imageURL={image.image}
-                            key={idx}
-                            title={image.title}
-                        />
-                    ))}
+                :
+                <div className='container'>
+                    <div className='error-section'>
+                        <h4>An error occured, please contact your network administrator.</h4>
+                    </div>
                 </div>
-            </div>
-        </div>
+            }
+        </>
+
     )
 }
