fc1d68fc7bd9dab31165a4c46c1e3277bacee2a7
aancjr <andrewancjr@gmail.com>
2022-10-26 20:26:00 -0400

    installed dependencies and created app based off wireframe.

---
+++ src/Components/Home.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ImageCard from './ImageCard';
+import styled from "styled-components";
+
+const Select = styled.select`
+  width: 50vw;
+  height: 35px;
+  padding-left: 5px;
+  font-size: 14px;
+  margin-left: 10px;
+  border-radius: 8px;
+
+  option {
+    color: black;
+    background: white;
+    display: flex;
+    white-space: pre;
+    min-height: 20px;
+    padding: 0px 2px 1px;
+  }
+`;
+
+export default function Home() {
+    const [data, setData] = useState([]);
+    const [filterOptions, setFilterOptions] = useState([]);
+    const [pickedFilter, setPickedFilter] = useState([]);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        axios.get('https://guileless-sopapillas-50e236.netlify.app/articles.json')
+            .then(results => {
+                setData(results.data);
+                let uniqueFilterOptions = [... new Set(results.data.map(obj => obj.category))]
+                setFilterOptions(uniqueFilterOptions);
+                console.log('hey')
+            })
+            .catch(err => setError(err));
+
+        return () => { }
+
+    }, [])
+
+    const handleChange = (e) => {
+        setPickedFilter(e.target.value);
+    };
+
+    return (
+        <div className='container'>
+            <div className="filter-section">
+                <div className="input-row">
+                    <form id="category-filter">
+                        <label >Filter By: </label>
+                        <Select id='images' name='images' onChange={handleChange}>
+                            <option key='default' value=''>All Categories</option>
+                            {filterOptions.map((category, idx) => { return <option key={idx} value={category}>{category}</option> })}
+                        </Select>
+                    </form>
+                </div>
+            </div>
+            <div className="image-section">
+                <div className="image-row">
+                    {data?.filter(obj => {
+                        return obj.category.includes(pickedFilter)
+                    }).map((image, idx) => (
+                        <ImageCard
+                            category={image.category}
+                            description={image.description}
+                            imageURL={image.image}
+                            key={idx}
+                            title={image.title}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/ImageCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+    border:none;
+`;
+
+export default function ImageCard(props) {
+    const { category, description, idx, imageURL, title } = props;
+
+
+    return (
+        <StyledCard key={idx}>
+            <img
+                className='card-image'
+                alt={`${title} - ${description}`}
+                src={imageURL}
+                title={title}
+            />
+            <h2>{title}</h2>
+            <div>
+                <p>{description}</p>
+            </div>
+        </StyledCard>
+    )
+}
